205b3673c70dc1737f182bbb776db9f860c7584d
Chris Gruber <chris@chrisgruber.com>
2021-11-05 16:35:21 -0500

    add eslint and editorconfig rules and reformat source code files to comply

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,43 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        static: './dist',
-        hot: true
-    },
-    devtool: 'inline-source-map',
-    entry: './src/app/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-			{
-				test: /\.css$/,
-				exclude: /node_modules/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.mp3$/,
-				exclude: /node_modules/,
-				use: [{ loader: 'file-loader' }]
-			}
-        ]
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ],
-    resolve: {
-		extensions: ['.ts', '.tsx', '.js']
-	}
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    hot: true
+  },
+  devtool: 'inline-source-map',
+  entry: './src/app/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.mp3$/,
+        exclude: /node_modules/,
+        use: [{loader: 'file-loader'}]
+      }
+    ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  }
 }
